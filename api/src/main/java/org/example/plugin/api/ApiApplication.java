f8284212831bc1619b52a951873d59d697dd04db
akomii <alexkombie@gmail.com>
2026-02-11 16:00:31 +0100

    added test child module

---
+++ api/src/main/java/org/example/plugin/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package main.java.org.example.plugin.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApiApplication.class, args);
+  }
+
+}
---
+++ api/src/test/java/org/example/plugin/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.plugin.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
