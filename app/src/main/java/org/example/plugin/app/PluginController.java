1b4ada27a0637c4ec3e8d2c5e2be5e95c3130460
akomii <alexkombie@gmail.com>
2026-03-11 10:23:27 +0100

    split pluginloader up into separate SRP classes

--- app/src/main/java/org/example/plugin/app/PluginController.java
+++ app/src/main/java/org/example/plugin/app/PluginController.java
@@ -2,7 +2,7 @@ package org.example.plugin.app;
 
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.List;
-import org.example.plugin.app.PluginLoader.PluginInfoDTO;
+import org.example.plugin.app.PluginModels.PluginInfoDTO;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
